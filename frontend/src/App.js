e4dcc05c1df321f1ffc488b42f3ab9b45acd311d
rylee816 <65700028+rylee816@users.noreply.github.com>
2022-05-17 18:05:10 -0400

    initial commit setup frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
