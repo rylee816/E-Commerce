6d1b04ca3f6aa62b2a1dbe1b66de9dfff8b6fbbd
rylee816 <65700028+rylee816@users.noreply.github.com>
2022-05-19 12:04:17 -0400

    Add reducer hook and conditional rendering for error and loading.

---
+++ frontend/src/reducers/fetchData.reducer.js
@@ -0,0 +1,14 @@
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'FETCH_REQUEST':
+            return {...state, loading: true};
+        case 'FETCH_SUCCESS': 
+            return {...state, products: action.payload, loading: false};
+        case 'FETCH_FAIL': 
+            return {...state, loading: false, error: action.payload};
+        default: 
+        return state;
+    }
+}
+
+export default reducer;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,27 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
+import fetchData from "../reducers/fetchData.reducer";
+import logger from "use-reducer-logger";
 
-
-// import data from "../data";
 
 function HomeScreen() {
-const [data, setData] = useState([])
+  const [{loading, error, products}, dispatch] = useReducer(logger(fetchData), {
+    products: [],
+    loading: true,
+    error: ''
+  })
 
-async function getData(){
-    const res = await fetch("http://localhost:3001/api/products")
-    const data = await res.json()
-    setData(data)
-}
-
-useEffect(() => {
-    getData()
-}, [])
+  useEffect(() => {
+    const fetchData = async function() {
+      dispatch({type: 'FETCH_REQUEST'})
+      try {
+        const result = await fetch("http://localhost:3001/api/products");
+        const data = await result.json();
+        dispatch({type: 'FETCH_SUCCESS', payload: data})
+      } catch(err){
+        dispatch({type: 'FETCH_FAIL', payload: err.message})
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.map((product) => (
+        {loading ? (
+          <div><h1>Loading...</h1></div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : 
+          products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
