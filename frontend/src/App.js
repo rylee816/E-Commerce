740f3cdaf5d9973109710076825a5e918d0cfb0b
rylee816 <65700028+rylee816@users.noreply.github.com>
2022-05-19 20:33:32 -0400

    Add react-bootstrap. Add Navbar and Footer.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
 import HomeScreen from "./screens/HomeScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">amazona</Link>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>amazona</Navbar.Brand>
+          </LinkContainer>
+          </Container>
+        </Navbar>
         </header>
         <main>
+        <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />}/>
             <Route path="/product/:slug" element={<ProductScreen/>}/>
           </Routes>
+        </Container>
         </main>
+        <footer>
+          <div className="text-center">All Rights Reserved</div>
+        </footer>
       </div>
     </Router>
   );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 import fetchData from "../reducers/fetchData.reducer";
 import logger from "use-reducer-logger";
 
-
 function HomeScreen() {
-  const [{loading, error, products}, dispatch] = useReducer(logger(fetchData), {
-    products: [],
-    loading: true,
-    error: ''
-  })
+  const [{ loading, error, products }, dispatch] = useReducer(
+    logger(fetchData),
+    {
+      products: [],
+      loading: true,
+      error: "",
+    }
+  );
 
   useEffect(() => {
-    const fetchData = async function() {
-      dispatch({type: 'FETCH_REQUEST'})
+    const fetchData = async function () {
+      dispatch({ type: "FETCH_REQUEST" });
       try {
         const result = await fetch("http://localhost:3001/api/products");
         const data = await result.json();
-        dispatch({type: 'FETCH_SUCCESS', payload: data})
-      } catch(err){
-        dispatch({type: 'FETCH_FAIL', payload: err.message})
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
       }
     };
     fetchData();
@@ -30,26 +32,29 @@ function HomeScreen() {
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
-          <div><h1>Loading...</h1></div>
+          <div>
+            <h1>Loading...</h1>
+          </div>
         ) : error ? (
           <div>{error}</div>
-        ) : 
+        ) : (
           products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
+            <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
+                <img src={product.image} alt={product.name} />
               </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to Cart</button>
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
