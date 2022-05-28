1539c5cd390267d0204bf7243222aebe40363081
rylee816 <65700028+rylee816@users.noreply.github.com>
2022-05-28 11:17:40 -0400

    Install mongoose and dotenv packages. Env file with URI. Connect to AtlasDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import data from './data.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to Atlas DB');
+})
+.catch(err => console.log(err.message))
 
 
 app.get("/api/products", (req, res) => {
