0a614a1842f26f2318cbf61df4d732f4e059b635
rylee816 <65700028+rylee816@users.noreply.github.com>
2022-05-31 13:02:48 -0400

    create User model with bcrypt. Seed database with Users.

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Product from '../models/productModel.js';
+import User from "../models/userModel.js";
 import data from '../data.js'
 
 const seedRouter = express.Router();
@@ -8,7 +9,10 @@ seedRouter.get('/', async (req, res) => {
     await Product.deleteMany({});
     const createdProducts = await Product.insertMany(data.products);
 
-    res.send({ createdProducts })
+    await User.deleteMany({});
+    const createdUsers = await User.insertMany(data.users);
+
+    res.send({ createdProducts, createdUsers });
 });
 
 export default seedRouter;
