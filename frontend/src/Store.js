f4435edbeb41d4a0160d6895572805694662ee3a
rylee816 <65700028+rylee816@users.noreply.github.com>
2022-06-02 19:53:07 -0400

    add persistence to shipping address on reload

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,29 +1,29 @@
-import React from 'react'
+import React from "react";
 import { createContext, useReducer } from "react";
-import cartReducer from './reducers/cartReducer.reducer';
+import cartReducer from "./reducers/cartReducer.reducer";
 
 export const Store = createContext();
 
 const initialState = {
-    userInfo: localStorage.getItem('userInfo')
-    ? JSON.parse(localStorage.getItem('userInfo'))
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
     : null,
 
-    cart: {
-        cartItems: localStorage.getItem('cartItems')
-         ?  JSON.parse(localStorage.getItem('cartItems'))
-         : []
-    }
+  cart: {
+    shippingAddress: localStorage.getItem("shippingAddress")
+      ? JSON.parse(localStorage.getItem("shippingAddress"))
+      : {},
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+  },
 };
 
-
 function StoreProvider(props) {
-    const [state, dispatch] = useReducer(cartReducer, initialState);
-    const value = {state, dispatch}
-    
-  return (
-    <Store.Provider value = {value}>{props.children}</Store.Provider>
-  )
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+  const value = { state, dispatch };
+
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
 }
 
-export default StoreProvider
+export default StoreProvider;
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -15,7 +15,7 @@ const [shippingState, setShippingState] = useState({
     country: '',
 })
 
-const {state, dispatch: contextDispatch} = useContext(Store);
+const {state: {cart: {shippingAddress}}, dispatch: contextDispatch} = useContext(Store);
 const navigate = useNavigate()
 
 const handleChange = (e) => {
@@ -46,9 +46,9 @@ const submitHandler = async (e) => {
         postalCode,
         country
     }));
-
     navigate('/payment');
-}
+};
+
 
   return (
       <>
@@ -63,7 +63,7 @@ const submitHandler = async (e) => {
             <Form.Control
             required
             name="fullName"
-            value={shippingState.fullName}
+            value={shippingState.fullName || shippingAddress.fullName}
             onChange={handleChange}
              />
         </Form.Group>
@@ -71,7 +71,7 @@ const submitHandler = async (e) => {
             <Form.Label>Street Address</Form.Label>
             <Form.Control
             required
-            value={shippingState.address}
+            value={shippingState.address || shippingAddress.address}
             name="address"
             onChange={handleChange}
              />
@@ -81,7 +81,7 @@ const submitHandler = async (e) => {
             <Form.Control
             required
             name="city"
-            value={shippingState.city}
+            value={shippingState.city || shippingAddress.city}
             onChange={handleChange}
              />
         </Form.Group>
@@ -90,7 +90,7 @@ const submitHandler = async (e) => {
             <Form.Control
             required
             name="state"
-            value={shippingState.state}
+            value={shippingState.state || shippingAddress.state}
             onChange={handleChange}
              />
         </Form.Group>
@@ -99,7 +99,7 @@ const submitHandler = async (e) => {
             <Form.Control
             required
             name="postalCode"
-            value={shippingState.postalCode}
+            value={shippingState.postalCode || shippingAddress.postalCode}
             onChange={handleChange}
              />
         </Form.Group>
@@ -108,7 +108,7 @@ const submitHandler = async (e) => {
             <Form.Control
             required
             name="country"
-            value={shippingState.country}
+            value={shippingState.country || shippingAddress.country}
             onChange={handleChange}
              />
         </Form.Group>
