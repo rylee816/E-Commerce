6422a6151ec7a3856d4b1d8a717be36b4d7a0877
rylee816 <65700028+rylee816@users.noreply.github.com>
2022-06-12 22:33:07 -0400

    set up heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import data from './data.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
@@ -7,6 +6,7 @@ import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -30,6 +30,12 @@ app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+})
 
 app.use((err, req, res, next) => {
     res.status(500).send({message: err.message});
